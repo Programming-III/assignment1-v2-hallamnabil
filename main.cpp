47de56d258f696ca96ff04f1be3c8d79db3fd99a
hallamnabil <halladoda@gmail.com>
2025-11-11 09:47:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,15 @@ using namespace std;
 
 // ==================== Person Class Implementation =========================
 
+::Person Person(string n, int i) {
+	name = n;
+	id = i;
+}
+void ::Person display() {
+	cout << "name: " << name << endl;
+	cout << "ID: " << id << endl;
 
+}
 
 
 
@@ -20,13 +28,34 @@ using namespace std;
 // ==================== Student Class Implementation ====================
 
 
+:: Student(string n, int i, string m, int y):person(n,i){
+major=m;
+yearLevel = y;
+}
+void :: Student void display() override{
+cout<< "student info: << endl;
+cout << name <<endl;
+cout << id<< endl;
+cout << major<< endl;
+cout << yearLevel << endl;
+}
 
 
 
 
 // ==================== Instructor Class Implementation ====================
 
-
+:: Instructor Instructor(string n, int i, string d, int e) : Person(n, i){
+   department =d;
+   experienceYears = e;
+}
+void:: Instructor display() override{
+cout<< "instructor info: " << endl;
+cout<< name<< endl;
+cout<< id<< endl;
+cout<< department<< endl;
+cout<< experienceYears<< endl;
+}
 
 
 
@@ -34,7 +63,34 @@ using namespace std;
 // ==================== Course Class Implementation ====================
 
 
-
+:: Course Course(string cc, string cn, int ms, Student* s){
+   courseCode = cc;
+   courseName = cn;
+   maxStudents = ms;
+   *students = new Student(s);
+   currentStudents=0;
+}
+void :: Course addStudent(const Student& s){
+   if(currentStudents==maxStudents)
+      cout << "max students reached" << endl;
+   else {
+      students[currentStudents+1] = s;
+      currentStudents++;
+   }
+}
+void:: Course displayCourseInfo(){
+   cout<< courseCode << endl;
+   cout<< courseName << endl;
+   cout<< maxStudents<< endl;
+   
+   for(int i=0; i<currentStudents; i++)
+      cout<<"currently enrolled" << students[i]<< endl;
+      
+   
+}
+:: Course ~Course(){
+   delete [] students;
+}
 
 
 
@@ -42,7 +98,11 @@ using namespace std;
 
 // ==================== Main Function ====================
 int main() {
-   
-    
+   Student s("Omar nabil" , 2202,"informatics" , 2 );
+   Instructor I("Dr. Lina Khaled", 1002, "computer science", 5);
+   Course c("CS101" , "Introduction to Programming", 3, Student* st);
+    s.display();
+   I.display();
+   c.displayCourseInfo();
     return 0;
 }
