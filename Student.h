e5326a10ddde502e07539f2a6ad21fc5a9c930b8
hallamnabil <halladoda@gmail.com>
2025-11-11 08:54:56 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -5,7 +5,13 @@
 #include <string>
 #using namespace std;
 //#write student class here
-
+class student : public person {
+    private:
+		string major;
+        int yearLevel;
+public: 
+	void display() override;
+};
 
 
 
